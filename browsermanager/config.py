ff54efd63ee0aa87ad6a05987fcfea9eeeae5011
Remy Kroese <rkroese96@gmail.com>
2023-03-11 13:51:18 +0100

    [test] config file 100% coverage

--- browsermanager/config.py
+++ browsermanager/config.py
@@ -1,29 +1,26 @@
 import configparser
 import os
 
-config = configparser.ConfigParser()
-CONFIG_PATH = 'config.ini'
 
+class Config():
+    def __init__(self, config_path='config.ini'):
+        self.config = configparser.ConfigParser()
+        self.config_path = config_path
+        if not os.path.isfile(self.config_path):
+            new_window = ['https://github.com/RemyKroese/browsermanager#readme']
+            self.write([new_window])
 
-def create():
-    window = ['https://github.com/RemyKroese/browsermanager#readme']
-    write([window])
+    def write(self, windows):
+        for i, window in enumerate(windows):
+            self.config.add_section('window_' + str(i+1))
+            for j, url in enumerate(window):
+                self.config['window_' + str(i+1)]['url_' + str(j+1)] = url
+        with open(self.config_path, 'w') as config_file:
+            self.config.write(config_file)
 
-
-def write(windows):
-    for i, window in enumerate(windows):
-        config.add_section('window_' + str(i+1))
-        for j, url in enumerate(window):
-            config['window_' + str(i+1)]['url_' + str(j+1)] = url
-    with open(CONFIG_PATH, 'w') as config_file:
-        config.write(config_file)
-
-
-def read():
-    if not os.path.isfile(CONFIG_PATH):
-        create()
-    config.read(CONFIG_PATH)
-    windows = []
-    for section in config.sections():
-        windows.append(list(config[section].values()))
-    return windows
+    def read(self):
+        self.config.read(self.config_path)
+        windows = []
+        for section in self.config.sections():
+            windows.append(list(self.config[section].values()))
+        return windows
