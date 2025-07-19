23774f34cea94f822b3ba1de5489f947a4e098de
Remy Kroese <rkroese96@gmail.com>
2025-07-19 21:31:11 +0200

    Make firefox defautl browser

--- browsermanager/browser.py
+++ browsermanager/browser.py
@@ -2,18 +2,25 @@ import subprocess
 import time
 
 CHROME_PATH = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+FIREFOX_PATH = r'C:\Program Files\Mozilla Firefox\firefox.exe'
 
 
-def open_startup_windows(windows):
+def open_startup_windows(windows, browser):
     for window in windows:
         if window['run_on_startup']:
-            open_window(window)
+            open_window(window, browser)
 
 
-def open_window(window):
+def open_window(window, browser):
+    if browser == 'firefox':
+        browser_path = FIREFOX_PATH
+    elif browser == 'chrome':
+        browser_path = CHROME_PATH
+    else:
+        browser_path = 'explorer'
     urls = window['urls']
-    subprocess.Popen([CHROME_PATH, urls[0], '--new-window'])
+    subprocess.Popen([browser_path, '--new-window', urls[0]])
     time.sleep(1)
     for i in range(1, len(urls)):
-        subprocess.Popen([CHROME_PATH, urls[i]])
-        time.sleep(0.1)
+        subprocess.Popen([browser_path, urls[i]])
+        time.sleep(0.2)
